93ce66bf42696ab4a5dafe947c74b261a859e6de
Joseph Wright <joseph.wright@morningstar2.co.uk>
2016-01-20 16:23:15 +0000

    Typo

--- build.lua
+++ build.lua
@@ -13,7 +13,7 @@ typesetfiles = {"*.dtx", "*.tex"}
 -- All-in-one .dtx file
 unpackfiles  = {"*.dtx"}
 
--- Only typeet the user part of the .dtx
+-- Only typeset the user part of the .dtx
 -- To avoid an issue with the .tex file, a bit of TeX coding
 typsetcmds = [[
   \\ifdefined\\OnlyDescription\\AtBeginDocument{\\OnlyDescription}\\fi
